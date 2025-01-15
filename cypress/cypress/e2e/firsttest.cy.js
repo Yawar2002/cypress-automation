9493acb0200c298279de7c8f52fa7a425f36ecea
yawarali <yawaralibalti617@gmail.com>
2025-01-15 18:43:15 +0500

    first commit

---
+++ cypress/cypress/e2e/firsttest.cy.js
@@ -0,0 +1,17 @@
+
+
+describe('Launch Application', () => {
+    it('Launch the test application', () => {
+      cy.visit('https://www.saucedemo.com')
+    })
+  })
+
+//   describe('Longin scenario', () => {
+//     it('Test1 - Valid login', () => {
+
+//   })
+
+//   it('Test2 - Valid login', () => {
+
+//     })
+// })   
